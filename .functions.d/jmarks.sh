5e2dd771206388e9a336cd0d0d0fd8a7a110c050
Andy Weidenbaum <archbaum@gmail.com>
2018-07-18 17:00:30 -0400

    put jmarks in ~/.config/jmarks

--- .functions.d/jmarks.sh
+++ .functions.d/jmarks.sh
@@ -2,26 +2,42 @@
 
 # -----------------------------------------------------------------------------
 # jmarks: quickly navigate your filesystem from the command-line
-# http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
+#
+# credit: http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
 # -----------------------------------------------------------------------------
 
-export MARKPATH=$HOME/.marks
+readonly MARKPATH="$HOME/.config/jmarks"
+
 jump() {
-  mkdir -p "$MARKPATH"; cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
+  mkdir -p "$MARKPATH"
+  cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
+
 mark() {
-  mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+  mkdir -p "$MARKPATH"
+  ln -s "$(pwd)" "$MARKPATH/$1"
 }
+
 unmark() {
-  mkdir -p "$MARKPATH"; rm -i "$MARKPATH/$1"
+  mkdir -p "$MARKPATH"
+  rm -i "$MARKPATH/$1"
 }
+
 marks() {
-  ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
+  find "$MARKPATH" -mindepth 1 -maxdepth 1 \( -type f -o -type l \) \
+    | sed 's,^\./,,'
 }
+
 _completemarks() {
-  local curw=${COMP_WORDS[COMP_CWORD]}
-  local wordlist=$(find $MARKPATH -type l -printf "%f\n")
-  COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
+  local curw
+  local wordlist
+  curw="${COMP_WORDS[COMP_CWORD]}"
+  wordlist="$(find "$MARKPATH" -type l -printf '%f\n')"
+  COMPREPLY=($(compgen -W "${wordlist[@]}" -- "$curw"))
   return 0
 }
+
 complete -F _completemarks jump unmark
+export MARKPATH
+
+# vim: set filetype=sh foldmethod=marker foldlevel=0 nowrap:
