f23fd488b7bae48b74302d22ee2bd82ee4a7c1ae
Andy Weidenbaum <archbaum@gmail.com>
2019-09-18 18:02:28 -0300

    add colordiff pgp fingerprint

--- fetch-pgp-keys.sh
+++ fetch-pgp-keys.sh
@@ -84,6 +84,9 @@ _keyids+=('01EA5486DE18A882D4C2684590C8019E36C2E964')
 # boost (https://www.boost.org/users/download/)
 _keyids+=('8756C4F765C9AC3CB6B85D62379CE192D401AB61')
 
+# colordiff (https://www.colordiff.org/)
+_keyids+=('CF3A93EF01E616C5AE7A1D2745E1E473378BB197')
+
 # cryfs (https://github.com/cryfs/cryfs/releases)
 _keyids+=('C3111FE656732704EF6DF059017DC932549E65B2')
 
