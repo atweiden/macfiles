617dd8b796fb06aa21424b5cab872adb48d5d5c9
Andy Weidenbaum <archbaum@gmail.com>
2020-01-03 09:59:14 +1300

    macos.sh: remove `tmutil disablelocal`
    https://github.com/mathiasbynens/dotfiles/pull/897

--- macos.sh
+++ macos.sh
@@ -656,10 +656,6 @@ defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# Disable Time Machine backups
-hash tmutil &> /dev/null \
-  && sudo tmutil disable
-
 # end time machine }}}
 # ==============================================================================
 # activity monitor {{{
