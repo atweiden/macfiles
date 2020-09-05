e236aaf541c2d6fb4eef0dde1c91af29ccc95f95
Andy Weidenbaum <atweiden@ioiojo.com>
2020-09-05 13:42:02 +1000

    normalize credit byline

--- .functions.d/qrshow.sh
+++ .functions.d/qrshow.sh
@@ -2,7 +2,8 @@
 
 # -----------------------------------------------------------------------------
 # qrshow: create and display QR code with qrencode
-# https://news.ycombinator.com/item?id=5332843
+#
+# Credit: https://news.ycombinator.com/item?id=5332843
 # -----------------------------------------------------------------------------
 
 qrshow() { qrencode -s 10 "$1" -o - | open -a Preview -f ; }
