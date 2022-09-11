a01139c5dd86d97d750ef4ae6de41a0bda4a7759
Andy Weidenbaum <atweiden@ioiojo.com>
2022-09-11 13:19:52 +1000

    focus follows mouse

--- macos.sh
+++ macos.sh
@@ -642,8 +642,8 @@ defaults write com.apple.terminal StringEncodings -array 4
 
 # Enable "focus follows mouse" for Terminal.app and all X11 apps
 # i.e. hover over a window and start typing in it without clicking first
-#defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true
+defaults write com.apple.terminal FocusFollowsMouse -bool true
+defaults write org.x.X11 wm_ffm -bool true
 
 # Enable Secure Keyboard Entry in Terminal.app
 # See: https://security.stackexchange.com/a/47786/8918
